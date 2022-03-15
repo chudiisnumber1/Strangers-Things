9de564baaecea1201b594a3a62185a2d38acf2b7
Sammy_Ester <sester2@outlook.com>
2022-03-15 13:31:57 -0500

    Minor changes to the post page

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from "react";
 import { fetchPosts } from "../api/index.js";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const getPosts = async () => {
-      const result = await fetchPosts();
-      console.log(result, "this is from useEffect in posts");
-      setPosts(result);
-    };
-    getPosts();
-  }, []);
-  return <div></div>;
+    useEffect(() => {
+        const getPosts = async () => {
+            const result = await fetchPosts();
+            console.log(result, "this is from useEffect in posts");
+            setPosts(result);
+        };
+        getPosts();
+    }, []);
+    return <div>
+
+    </div>;
 };
 export default Posts;
