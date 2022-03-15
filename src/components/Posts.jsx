8c3c7926b188c632765e9b1c54f995d9d643c5a5
Chudi <chudionubogu@yahoo.com>
2022-03-15 10:56:41 -0500

    more additional edits to posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { fetchPosts} from '../api/index.js';
-import { useEffect, UseState } from 'react';
+import { fetchPosts } from "../api/index.js";
+import { useEffect, UseState } from "react";
 import { useState } from "react/cjs/react.production.min";
 
-
-
 const Posts = () => {
-    const[posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-    useEffect() => {
-        
-    }
-
-    return <div>Post</div>;
+  useEffect(() => {
+    const getPosts = async () => {
+      const result = await fetchPosts();
+      const newPosts = result.data.posts;
+      setPosts(newPosts);
+    };
+    getPosts();
+  }, []);
+  return (
+    <div>
+      {posts.map((post, i) => {
+        return;
+      })}
+    </div>
+  );
 };
-
 export default Posts;
