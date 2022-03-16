0a2e7cf447dfae4631adb7d8e8d640d9b5914ba9
Chudi <chudionubogu@yahoo.com>
2022-03-16 12:13:59 -0500

    added postBox to posts.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Posts from "./Posts";
 
 const Home = () => {
   return <div>Home</div>;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,34 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { fetchPosts } from "../api/index.js";
 
-
-
-
 const Posts = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const getPosts = async () => {
-            const result = await fetchPosts();
-            console.log(result, "this is from useEffect in posts");
-            setPosts(result);
-        };
-        getPosts();
-    }, []);
-    console.log(posts)
-    return (
-        <div>
-            {posts.map((post, i) => {
-                return (
-                    <div>
-                        <h1>{post.title}</h1>
-                        <h1>{post.title}</h1>
-                    </div>
-                )
-            })
-            }
-        </div>
-    )
-
+  useEffect(() => {
+    const getPosts = async () => {
+      const result = await fetchPosts();
+      console.log(result, "this is from useEffect in posts");
+      setPosts(result);
+    };
+    getPosts();
+  }, []);
+  console.log(posts);
+  return (
+    <div>
+      {posts.map((post, i) => {
+        return (
+          <div className="postBox">
+            <h1>{post.title}</h1>
+            <h2>{post.author.username}</h2>
+            <h3>{post.description}</h3>
+            <h3>{post.price}</h3>
+            <h3>{post.location}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default Posts;
