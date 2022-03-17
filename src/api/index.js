8132270e7db3f5d03dcaab2dd26ea07bb08ef406
Sammy_Ester <sester2@outlook.com>
2022-03-17 12:56:01 -0500

    The additions to the API

--- src/api/index.js
+++ src/api/index.js
@@ -1,59 +1,60 @@
 //fetch methods, get, post, patch, delete.
 
 import Posts from "../components/Posts";
+const URL = "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT";
 
 export const fetchPosts = async () => {
   try {
-    const response = await fetch(
-      "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT/posts"
-    )
-      .then((response) => response.json())
-      .then((Posts) => {
-        console.log(Posts, "this is from fetch");
-      });
-    const data = response.data.posts;
-    console.log(data, "data from the api");
-    return data;
+    const response = fetch(`${URL}/Posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Bearer`${token}`,
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      },
+    });
+    const results = response.JSON();
+    return results.data.token;
   } catch (error) {
-    throw error;
+    console.log(error);
   }
 };
 
-export const sendPosts = async () => {
-  fetch("https://strangers-things.herokuapp.com/api/COHORT-NAME/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer ${token}",
-    },
-    body: JSON.stringify({
-      user: {
-        id,
-      },
-    }),
-  });
-  const data = response.data.posts;
-  console.log(data, "data from the api");
-  return data;
-};
+// export const sendPosts = async () => {
+//   fetch("https://strangers-things.herokuapp.com/api/COHORT-NAME/posts", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: "Bearer ${token}",
+//     },
+//     // body: JSON.stringify({
+//     //   user: {
+//     //     id,
+//     //   },
+//     // }),
+//   });
+//   const data = response.data.posts;
+//   console.log(data, "data from the api");
+//   return data;
+// };
 
 export const userSignUP = async (username, password) => {
   try {
-    const newUser = await fetch(
-      "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT/users/register",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    const newUser = await fetch(`${URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
         },
-        body: JSON.stringify({
-          user: {
-            username,
-            password,
-          },
-        }),
-      }
-    );
+      }),
+    });
     const result = await newUser.json();
     console.log(result.data.token);
     return result.data.token;
